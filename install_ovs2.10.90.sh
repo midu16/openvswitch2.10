d03526a818ac3dba672fa63504bb18cb05ed351f
Mihai I <idumihai16@gmail.com>
2019-04-26 12:52:44 +0200

    Update install_ovs2.10.90.sh

--- install_ovs2.10.90.sh
+++ install_ovs2.10.90.sh
@@ -33,26 +33,35 @@ echo_warning() { cecho "$*" $yellow       ;}
 echo_success() { cecho "$*" $green        ;}
 echo_info()    { cecho "$*" $blue         ;}
 
-check_supported_os_dist() {
-    case "$os_dist" in
-        "ubuntu17.10") return 0 ;;
-        "ubuntu17.04") return 0 ;;
-        "ubuntu16.04") return 0 ;;
-        "ubuntu14.04") return 0 ;;
-        "fedora24")    return 0 ;;
-        "rhel7")       return 0 ;;
-        "centos7")     return 0 ;;
-    esac
-    return 1
-}
 
 function install_ovs {
   echo "Installing the OpenvSwitch-2.10.90"
   git clone https://github.com/openvswitch/ovs.git
   OVS_PATH=$PWD
   cd $OVS_PATH/ovs
-  
+  git checkout origin/branch-2.10
+  ./boot.sh
+  ./configure
+  make
+  echo_info "Checking all the previous work done"
+  make check
+  echo_info "Start the installation!"
+  make install
+  cd -
 }
 
+#begin the installation process
+echo_info "Install all the aditional repositories needed to prepare the host environment!"
+sudo apt-get update -y 
+sudo apt-get install git -y
+sudo apt install build-essential libssl1.0.0 libcap-ng-utils -y
+sudo apt-get install python2.7 libnuma-dev libtool autoconf automake wget python-six libvirt-bin -y
+sudo apt-get install g++-4.9 gcc-multilib -y
+sudo apt-get install libpcap-dev -y
+sudo apt-get install iperf -y
 
-ORIGIN_PATH=$PWD
+echo_info "Start installing the ovs-2.10.90 on the host"
+install_ovs
+echo_success "The installation is done! The ovs-ctl will start"
+sudo ovs-ctl start
+
